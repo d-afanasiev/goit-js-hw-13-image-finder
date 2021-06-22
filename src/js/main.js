b851843ecf392f10500d5e9e488c018b23725849
Afanasiev Dmitriy <59284294+d-afanasiev@users.noreply.github.com>
2021-06-22 15:14:55 +0300

    change main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,33 +1,43 @@
 import ApiService from './apiService';
 import cards from '../partials/cards';
+import LoadMoreBtn from './load-more-btn';
 
 const refs = {
   formSearch: document.querySelector('.search-form'),
   containerGallery: document.querySelector('.gallery'),
-  btnLoadMore: document.querySelector('[data-value="load-more"]'),
+  // btnLoadMore: document.querySelector('[data-value="load-more"]'),
   gallery: document.querySelector('.gallery'),
 };
 
+const loadMoreBtn = new LoadMoreBtn({ selector: '[data-value="load-more"]', hidden: true });
 const apiService = new ApiService();
 
 refs.formSearch.addEventListener('submit', onSubmit);
 
-refs.btnLoadMore.addEventListener('click', onLoadMore);
+loadMoreBtn.refs.button.addEventListener('click', onLoadMore);
 
 function onSubmit(e) {
   e.preventDefault();
 
+  loadMoreBtn.show();
   apiService.query = e.currentTarget.elements.query.value;
   apiService.resetPage();
-  apiService.fetchImages().then(data => {
-    clearContainer();
-    appendGallery(data);
-  });
+  clearContainer();
+
+  fetchGallery();
 }
 
 function onLoadMore() {
-  apiService.fetchImages().then(appendGallery);
+  fetchGallery();
   setTimeout(scrollIntoView, 500);
+}
+
+function fetchGallery() {
+  loadMoreBtn.disable();
+  apiService.fetchImages().then(data => {
+    appendGallery(data);
+    loadMoreBtn.enable();
+  });
 }
 
 function appendGallery(result) {
